57ccdd42f7fc2124e7bb284beb5b0a9d0b8930a6
sipctodamoon <47186352+sipctodamoon@users.noreply.github.com>
2019-06-12 18:27:56 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,8 @@ else
     sed -i '$d' $CONFIGPATH/$CONFIGFILE
     sed -i '$d' $CONFIGPATH/$CONFIGFILE
     sed -i '$d' $CONFIGPATH/$CONFIGFILE
+    sed -i '$d' $CONFIGPATH/$CONFIGFILE
+    
     echo masternodeprivkey=$MNPRIVKEY >> $CONFIGPATH/$CONFIGFILE
     echo externalip=`curl ifconfig.co` >>$CONFIGPATH/$CONFIGFILE
     echo masternode=1 >> $CONFIGPATH/$CONFIGFILE
