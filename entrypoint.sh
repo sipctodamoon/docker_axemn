c697859b3ca48bf12cbd0ecfc643e4057e1aed42
sipctodamoon <47186352+sipctodamoon@users.noreply.github.com>
2019-06-14 23:01:06 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,15 +4,15 @@ export BINARYPATH=/root/.axecore
 export CONFIGPATH=/root/.axecore
 export CONFIGFILE=axe.conf
 
-if [ -z "$MNPRIVKEY" ] || [ -z "$BLSKEY" ]; then
-    echo "must provide a mn key and blskey"
+if [ -z "$BLSKEY" ]; then
+    echo "must provide a blskey"
 else
     sed -i '$d' $CONFIGPATH/$CONFIGFILE
     sed -i '$d' $CONFIGPATH/$CONFIGFILE
     sed -i '$d' $CONFIGPATH/$CONFIGFILE
     sed -i '$d' $CONFIGPATH/$CONFIGFILE
     
-    echo masternodeprivkey=$MNPRIVKEY >> $CONFIGPATH/$CONFIGFILE
+   
     echo externalip=`curl ifconfig.co` >>$CONFIGPATH/$CONFIGFILE
     echo masternode=1 >> $CONFIGPATH/$CONFIGFILE
     echo masternodeblsprivkey=$BLSKEY >> $CONFIGPATH/$CONFIGFILE
